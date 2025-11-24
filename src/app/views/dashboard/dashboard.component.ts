66d8963c40fa3be9fc6ad166e7be571644dbc674
blufix79 <salvatore.manc@gmail.com>
2025-11-24 08:59:32 +0100

    Aggiornamento formattazione date e miglioramento delle chiamate ai servizi per le interazioni e i contratti

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -15,6 +15,7 @@ export class DashboardComponent implements OnInit {
     private contractsService: ContractsService
   ) {}
   interventions: Intervention[];
+  interventionToBeScheduled: Intervention[];
   contracts: Contract[];
 
   ngOnInit() {
@@ -24,14 +25,21 @@ export class DashboardComponent implements OnInit {
 
   loadInterventions() {
     this.interventionsService
-      .get('rangeDays', '30')
+      .get('rangeDays', '30', null, 'desc')
       .subscribe((interventions: Intervention[]) => {
         this.interventions = interventions;
       });
+
+    this.interventionsService
+      .get('status', '3', null, 'desc')
+      .subscribe((interventions: Intervention[]) => {
+        this.interventionToBeScheduled = interventions;
+      });
   }
+
   loadContracts() {
     this.contractsService
-      .get('expiring', '30')
+      .get('expiring', '10', 'desc')
       .subscribe((contracts: Contract[]) => {
         this.contracts = contracts;
       });
